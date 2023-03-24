2e08ff83fb00fa40cd6a271dcf1f872fb3869b08
Doran <smith.doran@gmail.com>
2023-03-24 16:09:18 -0400

    small updates and npm i

--- client/src/globals.js
+++ client/src/globals.js
@@ -2,3 +2,5 @@ export const BASE_URL =
   process.env.NODE_ENV === 'production'
     ? `${window.location.origin}/api`
     : 'http://localhost:3001/api';
+
+// export const BASE_URL = 'https://wellness-while-wfh.herokuapp.com/api';
